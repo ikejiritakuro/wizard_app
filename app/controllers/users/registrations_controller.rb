627e361b3c59d11e78f1995cdf6d8d98c04fc7b5
ikejiritakuro <talangchikao165@gmail.com>
2021-07-26 19:19:12 +0900

    住所情報登録の実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(sign_up_params)
+      unless @user.valid?
+        render :new and return
+      end
+    session["devise.regist_data"] = { user: @user.attributes }
+    session["devise.regist_date"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+    render :new_address
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
